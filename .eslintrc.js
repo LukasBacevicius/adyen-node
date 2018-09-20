2e06c9f103b47ed9aa3adfcb2ac35f02d1ed65fb
Lukas Bacevicius <lukasbacevicius@Lukass-MacBook-Pro.local>
2018-09-20 16:17:32 +0200

    Initial commit - adds initial implementation of nodeJS client for calling adyen api service

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  "extends": "airbnb-base",
+  "rules": {
+    "indent": [1, 4],
+    "max-len": [
+      1,
+      120
+    ],
+    "no-underscore-dangle": [
+      0
+    ]
+  }
+};
